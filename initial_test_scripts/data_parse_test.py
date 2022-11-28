bb037eda1547d7cca1a7ca87bada3dd91b66da48
Falconier <jacobbullin@roadrunner.com>
2022-11-28 12:51:51 -0500

    initial_ideas_112820221251

--- initial_test_scripts/data_parse_test.py
+++ initial_test_scripts/data_parse_test.py
@@ -5,9 +5,28 @@ import io
 baudRate = 9600
 ser = serial.Serial('/dev/ttyS3', baudRate)
 print(ser.name)
+## skip first line in case we only capture a partial message
+ser.readline()
+## begin data collection loop
 while(True):
     datastring = ser.readline()
     ##trim escape characters
     datastring = datastring.decode()
-    print(datastring)
-    parsedata = pynmea2.GGA.parse(datastring)
+    parsedata = pynmea2.parse(datastring)
+    datatype = datastring[3:6]
+    print(datatype)
+    print(parsedata)
+    if (datatype == 'GGA'):
+        print(parsedata.lon)
+        print(parsedata.lat)
+        print(parsedata.timestamp)
+    elif (datatype == 'GSA'):
+        print('GSA')
+    elif (datatype == 'RMC'):
+        print('RMC')
+    elif (datatype == 'GSV'):
+        print('GSV')
+    else:
+        print(datastring)
+    print('------------------------------')
+    
